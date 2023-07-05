505b7f3fb50baf62b49addd91d8b1b1eb5be1258
AlejandraSalinas <115848421+AlejandraSalinas@users.noreply.github.com>
2023-07-04 21:26:16 -0500

    no terminado

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('index', function () {
+    return view('Users.index');
 });
 
 Route::resource('usuarios', UserController::class);
-
